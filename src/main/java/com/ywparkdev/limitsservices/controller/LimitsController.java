6a946bc42cf95b5e5bc9cfed477db029c6a1ebfb
Yeon Woo Park <ywoo1234@gmail.com>
2021-02-28 17:41:09 -0500

    Add base configuration. Read property values from Configuration component

--- src/main/java/com/ywparkdev/limitsservices/controller/LimitsController.java
+++ src/main/java/com/ywparkdev/limitsservices/controller/LimitsController.java
@@ -1,15 +1,20 @@
 package com.ywparkdev.limitsservices.controller;
 
 import com.ywparkdev.limitsservices.bean.Limits;
+import com.ywparkdev.limitsservices.config.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
 
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("/limits")
     public Limits retrieveLimits() {
-        return new Limits(1, 1000);
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
     }
 
 }
