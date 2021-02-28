cb15229a68922c5470156161a6813180f1109c75
Yeon Woo Park <ywoo1234@gmail.com>
2021-02-28 17:18:52 -0500

    Initial commit for Limit Service

---
+++ src/test/java/com/ywparkdev/limitsservices/LimitsServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ywparkdev.limitsservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
